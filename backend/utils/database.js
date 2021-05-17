4475b724b832baf3d897cdc373bede797ecb6d4b
Rafał Krukowski <kruku13@gmail.com>
2021-05-17 13:25:56 +0200

    Add var env

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
+require('dotenv').config({ path: '.env' });
 
-const sequelize = new Sequelize('hrp', 'root', 'admin', {
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost',
+  host: process.env.MYSQL_ROOT_HOST,
 });
 
 module.exports = sequelize;
